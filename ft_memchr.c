c1bb0f1ffbb7e1a7d451ad7ce54ccf76da9f66d9
Wisani KHOSA <wkhosa@e5r8p11.wethinkcode.co.za>
2017-09-05 17:05:58 +0200

    additional part 1 funtions

---
+++ ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wkhosa <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/05 16:16:29 by wkhosa            #+#    #+#             */
+/*   Updated: 2017/09/05 16:30:02 by wkhosa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	int					index;
+	unsigned	char 	*str;
+
+	index = 0;
+	str = (unsigned char *)s;
+	while (n--)
+	{
+		if (str[index] == (unsigned char)c)
+			return ((unsigned char *)str + index);
+		index++;
+	}
+
+	return (NULL);
+}
